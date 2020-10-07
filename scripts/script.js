43940fe0ca6d01922dcbf324c84a9081772e5039
mikolaj <mikolajsve@gmail.com>
2020-10-07 16:04:35 +0200

    add animation on scroll

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,10 @@
 const showTestButtons = document.querySelectorAll('.test__click');
+const elementsToShow = document.querySelectorAll('.block__code');
+const scroll =
+  window.requestAnimationFrame ||
+  function (callback) {
+    window.setTimeout(callback, 1000 / 60);
+  };
 
 showTestButtons.forEach(function (button) {
   button.addEventListener('click', function () {
@@ -17,3 +23,30 @@ showTestButtons.forEach(function (button) {
     }
   });
 });
+
+function loop() {
+  elementsToShow.forEach((element) => {
+    if (isElementInViewport(element)) {
+      element.classList.add('is-visible');
+    } else {
+      element.classList.remove('is-visible');
+    }
+  });
+  scroll(loop);
+}
+
+loop();
+
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0 && rect.bottom >= 0) ||
+    (rect.bottom >=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <=
+        (window.innerHeight || document.documentElement.clientHeight)) ||
+    (rect.top >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
